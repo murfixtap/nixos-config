b8348d1df9da25904de4f59059113e24b3da55ba
murfixtap <murfixtap@gmail.com>
2026-02-24 00:51:16 +0200

    feat(fish): add comprehensive git and system abbreviations

--- home-manager/modules/shell/fish.nix
+++ home-manager/modules/shell/fish.nix
@@ -6,7 +6,7 @@
       set -g fish_greeting
     '';
 
-    shellAliases = {
+    shellAbbrs = {
       # System
       n = "nvim";
       cat = "bat";
@@ -19,9 +19,74 @@
       lt = "lsd --tree";
 
       # nh
-      nswitch = "nh os switch";
-      nupdate = "nh os switch --update";
-      nhome = "nh home switch";
+      nos = "nh os switch";
+      nou = "nh os switch --update";
+      nhm = "nh home switch";
+
+      # --- Git ---
+      # Main
+      g = "git";
+      gs = "git status";
+      ga = "git add";
+      gaa = "git add --all";
+      gc = "git commit";
+      gcmsg = "git commit -m";
+      gca = "git commit --amend";
+      gcan = "git commit --amend --no-edit";
+      gcam = "git commit -am";
+
+      # Branches
+      gb = "git branch";
+      gbr = "git branch --remote";
+      gco = "git checkout";
+      gcb = "git checkout -b";
+      gbd = "git branch -d";
+      gbD = "git branch -D";
+      gm = "git merge";
+
+      # Logs and diffs
+      gl = "git log --oneline";
+      glog = "git log --oneline --graph --decorate";
+      glg = "git log --graph --pretty=format:'%C(yellow)%h%Creset %s %C(blue)%an%Creset %C(green)%cd%Creset' --date=short";
+      gd = "git diff";
+      gds = "git diff --staged";
+
+      # Remote repositories
+      gps = "git push";
+      gpsup = "git push --set-upstream origin HEAD";
+      gpl = "git pull";
+      gplr = "git pull --rebase";
+      gf = "git fetch";
+      gfo = "git fetch origin";
+      grv = "git remote -v";
+      gra = "git remote add";
+      grrm = "git remote remove";
+
+      # Cancellation and Restore
+      grh = "git reset HEAD";
+      grhh = "git reset --hard HEAD";
+      grs = "git reset --soft HEAD~1";
+      grev = "git revert";
+
+      # Stash
+      gst = "git stash";
+      gstl = "git stash list";
+      gsta = "git stash apply";
+      gstp = "git stash pop";
+      gstd = "git stash drop";
+
+      # Rewriting history
+      grb = "git rebase";
+      grba = "git rebase --abort";
+      grbc = "git rebase --continue";
+      grbi = "git rebase -i";
+      gcp = "git cherry-pick";
+
+      # Other
+      gcl = "git clone";
+      gclean = "git clean -fd";
+      gbl = "git blame";
+      gignore = "git update-index --assume-unchanged";
     };
   };
 }
