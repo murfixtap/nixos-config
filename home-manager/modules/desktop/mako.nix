e2e13d56ac663d69d87ab4d1f943dfea31ec2da8
murfixtap <murfixtap@gmail.com>
2026-02-27 01:10:03 +0200

    fix(mako): migrate to settings to resolve deprecation warnings

--- home-manager/modules/desktop/mako.nix
+++ home-manager/modules/desktop/mako.nix
@@ -1,22 +1,23 @@
-{ ... }: {
+{...}: {
   services.mako = {
     enable = true;
+    settings = {
+      #appearance
+      # background-color = "";
+      # text-color = "";
+      # border-color = "";
+      border-size = 3;
+      width = 300;
+      padding = "10";
+      margin = "10";
+      border-radius = 5;
 
-    #appearance
-    # backgroundColor = "";
-    # textColor = "";
-    # borderColor = "";
-    borderSize = 3;
-    width = 300;
-    padding = "10";
-    margin = "10";
-    borderRadius = 5;
+      #behavior
+      default-timeout = 5000;
+      max-visible = 3;
 
-    #behavior
-    defaultTimeout = 5000;
-    maxVisible = 3;
-
-    #position
-    anchor = "top-right";
+      #position
+      anchor = "top-right";
+    };
   };
 }
