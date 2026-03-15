c50d754a3f5f0ebca1b75ff78dee22a5e1bc728d
murfixtap <murfixtap@gmail.com>
2026-03-15 04:22:09 +0200

    feat(audio): enhance pipewire config and enable rtkit

--- nixos/modules/core/audio.nix
+++ nixos/modules/core/audio.nix
@@ -1,8 +1,12 @@
 {...}: {
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
     wireplumber.extraConfig = {
       "10-disable-suspend" = {
         "monitor.alsa.rules" = [
