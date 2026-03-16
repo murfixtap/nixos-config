71d45df515c09bb0c3d135e406c70641367cdecf
murfixtap <murfixtap@gmail.com>
2026-03-16 16:52:01 +0200

    feat(nixos): enable libvirt and virt-manager

--- nixos/modules/core/user.nix
+++ nixos/modules/core/user.nix
@@ -13,9 +13,30 @@
       "audio"
       "docker"
       "render"
+      "libvirtd"
     ];
   };
 
-  programs.fish.enable = true;
-  virtualisation.docker.enable = true;
+  programs = {
+    fish.enable = true;
+    dconf.enable = true;
+    virt-manager.enable = true;
+  };
+
+  virtualisation = {
+    docker.enable = true;
+    libvirtd.enable = true;
+  };
+
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+      polkit.addRule(function(action, user) {
+        if (action.id == "org.libvirt.unix.manage" &&
+            user.isInGroup("libvirtd")) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+  };
 }
