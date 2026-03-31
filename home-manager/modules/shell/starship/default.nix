cd6cff2b67430f29eb3714fecc76b8aa24cdc3b1
murfixtap <murfixtap@gmail.com>
2026-03-31 20:39:15 +0300

    chore(starship): update prompt symbols

--- home-manager/modules/shell/starship/default.nix
+++ home-manager/modules/shell/starship/default.nix
@@ -6,9 +6,10 @@
     settings =
       (fromTOML (builtins.readFile ./nerd-font-symbols.toml))
       // {
+        add_newline = false;
         character = {
-          success_symbol = "[ ](bold blue)";
-          error_symbol = "[ ](bold red)";
+          success_symbol = "[󱞩](bold blue)";
+          error_symbol = "[󱞩](bold red)";
         };
       };
   };
